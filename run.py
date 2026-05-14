aef0b16dc88850f9105d86c2d41077463545c529
Diyer Diaz <diyerdiaz7@gmail.com>
2026-05-14 10:38:03 -0500

    cambios con docker

--- run.py
+++ run.py
@@ -30,4 +30,4 @@ with app.app_context():
 
 if __name__ == '__main__':
     #+++++++++++socketio.run(app, host="0.0.0.0", port=80, debug=True, allow_unsafe_werkzeug=True)
-    app.run(debug=True, host='0.0.0.0', port=81)
+    app.run(host="127.0.0.1", port=5001, debug=True)
